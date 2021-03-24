61b16c4b4ea5d79e21bcb8656f0aeb405ed01b43
Igor-Maz <beltane.eu@gmail.com>
2021-03-24 23:59:59 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    'todo': {
+      title: 'Todo',
+      items: []
+    },
+    'in-progress': {
+      title: 'In Progress',
+      items: []
+    },
+    'done': {
+      title: 'Completed',
+      items: []
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <h1>test</h1>
-      </div>
+
     </div>
 
   );
